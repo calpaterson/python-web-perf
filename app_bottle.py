8c4930a78773dbb3e2a309763bcd479d30f36fc5
Cal Paterson <cal@calpaterson.com>
2020-05-11 17:13:41 +0100

    Convert the bottle app

--- app_bottle.py
+++ app_bottle.py
@@ -8,4 +8,15 @@ app = bottle.Bottle()
 @app.route("/test")
 def test():
     a, b = get_row()
-    return json.dumps({"a": str(a).zfill(10), "b": b})
+    x, y = get_row()
+    z, w = get_row()
+    return json.dumps(
+        {
+            "a": str(a).zfill(10),
+            "b": b,
+            "x": str(x).zfill(10),
+            "y": y,
+            "z": str(z).zfill(10),
+            "w": w,
+        }
+    )
