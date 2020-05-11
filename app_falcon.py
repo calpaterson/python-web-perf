c0000842d6d88bf2ce87bdaf99b9c7733e9c4011
Cal Paterson <cal@calpaterson.com>
2020-05-11 17:28:38 +0100

    Have falcon use async_db

--- app_falcon.py
+++ app_falcon.py
@@ -1,32 +1,8 @@
 import falcon
-import psycopg2
-import psycopg2.pool
 import json
 import random
 
-pool = None
-
-def get_pool():
-    global pool
-    if pool is None:
-        pool = psycopg2.pool.SimpleConnectionPool(
-            4, 8, database="test", user="test", password="test", port=6432,
-        )
-    return pool
-
-
-max_n = 1000_000 - 1
-
-
-def get_row():
-    conn = get_pool().getconn()
-    cursor = conn.cursor()
-    index = random.randint(1, max_n)
-    cursor.execute("select a, b from test where a = %s;", (index,))
-    ((a, b),) = cursor.fetchall()
-    cursor.close()
-    get_pool().putconn(conn)
-    return a, b
+from sync_db import get_row
 
 
 class ThingResource:
