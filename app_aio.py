ed8296bed49ee5ff903ed2b2e2a1b89c0cf9c373
Cal Paterson <cal@calpaterson.com>
2020-05-11 21:49:26 +0100

    Fix aio app

--- app_aio.py
+++ app_aio.py
@@ -5,8 +5,7 @@ import aiopg
 from async_db import get_row
 
 async def handle(request):
-    ((a, b),) = await get_row()
-
+    a, b = await get_row()
     return web.Response(text=json.dumps({"a": str(a).zfill(10), "b": b}))
 
 
