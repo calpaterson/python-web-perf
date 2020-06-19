10457efa1ea53217a0f4db824af3e63de403e8f7
Oliver Russell <ojhrussell@gmail.com>
2020-06-19 11:04:16 +0100

    Revert "try asyncpg"
    This reverts commit bd63458a2defe23e163d552b2f45f127de09f0e5.

--- async_db.py
+++ async_db.py
@@ -1,12 +1,13 @@
 import random
-import asyncpg
+import aiopg
 
 pool = None
 
 async def get_pool():
     global pool
     if pool is None:
-        pool = await asyncpg.create_pool("postgres://test:test@localhost:6432/test")
+        pool = await aiopg.create_pool(
+            "dbname=test user=test password=test port=6432 host=127.0.0.1")
     return pool
 
 max_n = 1000_000 - 1
@@ -14,6 +15,8 @@ max_n = 1000_000 - 1
 async def get_row():
     pool = await get_pool()
     async with pool.acquire() as conn:
-        index = random.randint(1, max_n)
-        ((a, b),) = await conn.fetch(f"select a, b from test where a = {index}")
+        async with conn.cursor() as cursor:
+            index = random.randint(1, max_n)
+            await cursor.execute("select a, b from test where a = %s", (index,))
+            ((a, b),) = await cursor.fetchall()
     return a, b
